f72bbbccfd7e8ecbf8711a8697c80bdc62c12095
armsone <armsone@gmail.com>
2026-08-25 14:14:48 +0900

    release: prepare Android 2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,9 @@ android {
         applicationId = "com.armsone.starmanager"
         minSdk = 26
         targetSdk = 37
-        versionCode = 340540
+        versionCode = 340680
         versionName = "2.0.1"
-        buildConfigField("String", "BUILD_NUMBER", "\"202608251140\"")
+        buildConfigField("String", "BUILD_NUMBER", "\"202608251400\"")
     }
 
     buildTypes {
